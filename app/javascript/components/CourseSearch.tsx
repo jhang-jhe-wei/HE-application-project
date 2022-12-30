f46010f13ee388ec386f07c32bd96ab85239a228
wells <jhang0912407249@gmail.com>
2022-12-31 00:49:22 +0100

    style: add home layout

---
+++ app/javascript/components/CourseSearch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CourseSearch = () => {
+  return (
+    <div>
+      <input type="text" className="block w-full p-4 placeholder-gray-400 border border-gray-100 rounded-md bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"/>
+        <div className="mt-4 bg-white rounded-md h-96">
+          pending
+        </div>
+      </div>
+    )
+}
+
+export default CourseSearch;
---
+++ app/javascript/components/TimeTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TimeTable = () => {
+  return (
+    <div className="w-full h-full bg-white">
+      TimeTable
+    </div>
+  )
+}
+
+export default TimeTable;
