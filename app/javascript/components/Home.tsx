cd2478dcf2d59cfc747b35830e6609d75ee2049c
wells <jhang0912407249@gmail.com>
2023-01-04 00:06:15 +0100

    chore: add reducer and context

--- app/javascript/components/Home.tsx
+++ app/javascript/components/Home.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { mountToWindow } from '../helpers/helper';
 import CourseSearch from './CourseSearch';
 import TimeTable from './TimeTable';
 import SelectedCoursesList from './SelectedCoursesList';
+import CourseContext from '../courseContext';
+import { default as CourseReducer, initReducer } from '../courseReducer';
 
 const Home = () => {
+  const [state, dispatch] = useReducer(CourseReducer, 0, initReducer)
+
   return (
-    <div className="container py-12 mx-auto">
-      <div className="grid grid-cols-4 gap-4">
-        <div className="col-span-1">
-          <CourseSearch/>
-          <SelectedCoursesList/>
-        </div>
-        <div className="col-span-3">
-          <TimeTable/>
+    <CourseContext.Provider value={[state, dispatch]}>
+      <div className="container py-12 mx-auto">
+        <div className="grid grid-cols-4 gap-4">
+          <div className="col-span-1">
+            <CourseSearch/>
+            <SelectedCoursesList/>
+          </div>
+          <div className="col-span-3">
+            <TimeTable/>
+          </div>
         </div>
       </div>
-    </div>
+    </CourseContext.Provider>
   )
 }
 
--- app/javascript/courseReducer.ts
+++ app/javascript/courseReducer.ts
@@ -18,10 +18,48 @@ export interface CourseGroupState {
   events: GroupEventState[];
 }
 
-export interface CourseState {
+export interface Coursetate {
   id: number;
   name: string;
   class_url: string;
   semester: string;
   groups: CourseGroupState[];
 }
+
+export interface SelectedCourseState {
+  id: number;
+  className: string;
+  groupName: string;
+}
+
+export interface ReducerStateProps {
+  selectedCourseList: SelectedCourseState[];
+}
+
+export const ReducerActions = {
+  SET_SELECTED_COURSE_LIST: 'course$set_selected_course_list',
+} as const
+
+export type ReducerActionProps =
+    | { type: typeof ReducerActions.SET_SELECTED_COURSE_LIST; payload: SelectedCourseState[] }
+
+export const initReducer = (): ReducerStateProps => {
+  return {
+    selectedCourseList: []
+  }
+}
+
+const Reducer = (state: ReducerStateProps, action: ReducerActionProps) => {
+  switch (action.type) {
+    case ReducerActions.SET_SELECTED_COURSE_LIST: {
+      return {
+        ...state,
+        selectedCourseList: action.payload
+      };
+    }
+    default:
+      return state
+  }
+}
+
+export default Reducer;
