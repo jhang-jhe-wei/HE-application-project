8580c68862c6755be62e02289078f8835cc41c07
wells <jhang0912407249@gmail.com>
2023-01-06 12:00:32 +0100

    fix: event doesn't have full height

--- app/javascript/components/TimeTable/Event.tsx
+++ app/javascript/components/TimeTable/Event.tsx
@@ -47,7 +47,7 @@ const Event = (props: EventProps) => {
     }>
       <a
         href="#"
-        className={classNames("absolute flex flex-col p-2 overflow-y-auto text-xs rounded-lg inset-x-2 group bg-blue-50 leading-5 hover:bg-blue-100", { 'bg-red-300': hasConflict })}
+        className={classNames("absolute flex flex-col p-2 overflow-y-auto text-xs rounded-lg inset-x-2 group bg-blue-50 leading-5 hover:bg-blue-100 h-full", { 'bg-red-300': hasConflict })}
       >
         <p className="order-1 font-semibold text-blue-700">{ courseName }</p>
         <p className="text-blue-500 group-hover:text-blue-700">
