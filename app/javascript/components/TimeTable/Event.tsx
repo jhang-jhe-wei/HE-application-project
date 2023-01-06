31c9b884bb63a25a5bcf5105ed14a276c194a385
wells <jhang0912407249@gmail.com>
2023-01-06 08:29:47 +0100

    Refactor: Extract event to component

---
+++ app/javascript/components/TimeTable/Event.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {GroupEventState} from '../../courseReducer';
+
+const calculateGridRowValue = (event: GroupEventState) => {
+  const position = Math.floor((event.startedMinuteAt - 7 * 60) / 5) + 1
+  const span = Math.round((event.endedMinuteAt - event.startedMinuteAt) / 5)
+  return `${position} / span ${span}`
+}
+
+const calculateTimeDuration = (event: GroupEventState) => {
+  const sHour = Math.floor(event.startedMinuteAt/60);
+  const sMinute = (event.startedMinuteAt % 60).toString().padStart(2, '0');
+  const eHour = Math.floor(event.endedMinuteAt/60);
+  const eMinute = (event.endedMinuteAt % 60).toString().padStart(2, '0');
+  return `${sHour}:${sMinute}~${eHour}:${eMinute}`
+}
+
+const calculateColStartValue = (wday: number) => {
+  if(wday === 0) return 'col-start-7';
+  return `col-start-${wday}`
+}
+
+interface EventProps {
+  event: GroupEventState;
+  courseName: string;
+}
+
+const Event = (props: EventProps) => {
+  const {
+    event,
+    courseName
+  } = props;
+
+  return (
+    <li className={`relative flex mt-px sm:${calculateColStartValue(event.wday)}`} style={{ gridRow: `${calculateGridRowValue(event)}` }}>
+      <a
+        href="#"
+        className="absolute inset-0 flex flex-col p-2 overflow-y-auto text-xs rounded-lg group bg-blue-50 leading-5 hover:bg-blue-100"
+      >
+        <p className="order-1 font-semibold text-blue-700">{ courseName }</p>
+        <p className="text-blue-500 group-hover:text-blue-700">
+          { calculateTimeDuration(event) }
+        </p>
+      </a>
+    </li>
+  )
+}
+
+export default Event;
--- app/javascript/components/TimeTable/Events.tsx
+++ app/javascript/components/TimeTable/Events.tsx
@@ -1,26 +1,7 @@
 import React, {useContext} from 'react';
 import CourseContext from '../../courseContext';
 import { TIME_LIST } from ".";
-import {GroupEventState} from '../../courseReducer';
-
-const calculateGridRowValue = (event: GroupEventState) => {
-  const position = Math.floor((event.startedMinuteAt - 7 * 60) / 5) + 1
-  const span = Math.round((event.endedMinuteAt - event.startedMinuteAt) / 5)
-  return `${position} / span ${span}`
-}
-
-const calculateTimeDuration = (event: GroupEventState) => {
-  const sHour = Math.floor(event.startedMinuteAt/60);
-  const sMinute = (event.startedMinuteAt % 60).toString().padStart(2, '0');
-  const eHour = Math.floor(event.endedMinuteAt/60);
-  const eMinute = (event.endedMinuteAt % 60).toString().padStart(2, '0');
-  return `${sHour}:${sMinute}~${eHour}:${eMinute}`
-}
-
-const calculateColStartValue = (wday: number) => {
-  if(wday === 0) return 'col-start-7';
-  return `col-start-${wday}`
-}
+import Event from './Event';
 
 const Events = () => {
   const [state, ] = useContext(CourseContext)
@@ -33,21 +14,11 @@ const Events = () => {
       {
         state.selectedCourseList.map(course => (
           course.events.map(event => (
-            <li key={event.id} className={`relative flex mt-px sm:${calculateColStartValue(event.wday)}`} style={{ gridRow: `${calculateGridRowValue(event)}` }}>
-            <a
-              href="#"
-              className="absolute inset-0 flex flex-col p-2 overflow-y-auto text-xs rounded-lg group bg-blue-50 leading-5 hover:bg-blue-100"
-            >
-              <p className="order-1 font-semibold text-blue-700">{ course.className }</p>
-              <p className="text-blue-500 group-hover:text-blue-700">
-                { calculateTimeDuration(event) }
-              </p>
-            </a>
-          </li>
-        ))
+            <Event event={event} courseName={course.className}/>
+            ))
         ))
       }
-</ol>
+    </ol>
   )
 }
 
